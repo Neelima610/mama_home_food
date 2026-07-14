ffa5f8c8614455dd21550395d5439ab8fc1d62e6
Neelima <neelimaa5747@gmail.com>
2026-07-14 22:57:37 +0530

    chore: create feature-first project architecture

--- lib/shared/widgets/common/widgets.dart
+++
@@ -1,8 +0,0 @@
-
-export 'app_loader.dart';
-export 'app_logo.dart';
-export 'custom_app_bar.dart';
-export 'custom_button.dart';
-export 'custom_text_field.dart';
-export 'custom_card.dart';
-export 'section_title.dart';
